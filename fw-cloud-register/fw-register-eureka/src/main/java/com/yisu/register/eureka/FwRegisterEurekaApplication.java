b32916b90189b9b8e4044d79efaf08ef23692749
xuyisu <yisu.xu@baozun.com>
2019-12-06 16:55:37 +0800

    版本升级及eureka 调整

---
+++ fw-cloud-register/fw-register-eureka/src/main/java/com/yisu/register/eureka/FwRegisterEurekaApplication.java
@@ -0,0 +1,20 @@
+package com.yisu.register.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ *
+ * @Author xuyisu
+ * @Date 2019/12/6
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class FwRegisterEurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FwRegisterEurekaApplication.class, args);
+    }
+
+
+}
