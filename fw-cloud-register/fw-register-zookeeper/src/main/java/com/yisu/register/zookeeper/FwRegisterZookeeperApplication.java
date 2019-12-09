d32701d9bff7320ba126a15803b1fb600f88cedd
xuyisu <yisu.xu@baozun.com>
2019-12-09 15:48:36 +0800

    SpringCloud Zookeeper注册中心

---
+++ fw-cloud-register/fw-register-zookeeper/src/main/java/com/yisu/register/zookeeper/FwRegisterZookeeperApplication.java
@@ -0,0 +1,20 @@
+package com.yisu.register.zookeeper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ *
+ * @Author xuyisu
+ * @Date 2019/12/6
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FwRegisterZookeeperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FwRegisterZookeeperApplication.class, args);
+    }
+
+
+}
