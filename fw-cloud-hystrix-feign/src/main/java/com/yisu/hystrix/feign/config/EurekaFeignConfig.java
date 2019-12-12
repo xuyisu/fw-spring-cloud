1149cc4b0dd9c0c8173ae423764ad45db2903590
xuyisu <yisu.xu@baozun.com>
2019-12-12 16:54:17 +0800

    turbine

---
+++ fw-cloud-hystrix-feign/src/main/java/com/yisu/hystrix/feign/config/EurekaFeignConfig.java
@@ -0,0 +1,33 @@
+package com.yisu.hystrix.feign.config;
+
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author xuyisu
+ * @description ribbon配置
+ * @date 2019/12/10
+ */
+@Configuration
+public class EurekaFeignConfig {
+
+    /**
+     * 解决dashboard显示 Unable to connect to Command Metric Stream
+     */
+    @Bean
+    public ServletRegistrationBean getServlet() {
+        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
+        ServletRegistrationBean registrationBean = new ServletRegistrationBean(streamServlet);
+        registrationBean.setLoadOnStartup(1);
+        registrationBean.addUrlMappings("/hystrix.stream");
+        registrationBean.setName("HystrixMetricsStreamServlet");
+        return registrationBean;
+    }
+
+}
---
+++ fw-cloud-turbine/src/main/java/com/yisu/turbine/FwTurbineApplication.java
@@ -0,0 +1,22 @@
+package com.yisu.turbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+/**
+ * 基于feign
+ * @Author xuyisu
+ * @Date 2019/12/6
+ */
+@EnableTurbine
+@EnableDiscoveryClient
+@SpringBootApplication
+public class FwTurbineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FwTurbineApplication.class, args);
+    }
+
+
+}
