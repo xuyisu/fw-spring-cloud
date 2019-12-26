3b1a19fd8513df60001a78e54efba79eb9cad32c
xuyisu <yisu.xu@baozun.com>
2019-12-26 17:44:00 +0800

    nacos 引入

---
+++ fw-cloud-config-center/fw-cloud-nacos-client/src/main/java/com/yisu/FwNacosClientApplication.java
@@ -0,0 +1,17 @@
+package com.yisu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author xuyisu
+ * @description
+ * @date 2019/12/12
+ */
+//@EnableDiscoveryClient
+@SpringBootApplication
+public class FwNacosClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FwNacosClientApplication.class, args);
+    }
+}
---
+++ fw-cloud-config-center/fw-cloud-nacos-client/src/main/java/com/yisu/nacos/client/TestController.java
@@ -0,0 +1,24 @@
+package com.yisu.nacos.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author xuyisu
+ * @description
+ * @date 2019/12/26
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class TestController {
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
