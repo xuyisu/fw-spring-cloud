829ac54313ba8fa326ed121bab8992a65e00f98d
bore <www.964092342@qq.com>
2019-12-31 17:24:36 +0800

    turbine

--- fw-cloud-hystrix/fw-cloud-hystrix-turbine/src/main/java/com/yisu/turbine/FwTurbineApplication.java
+++ fw-cloud-hystrix/fw-cloud-hystrix-turbine/src/main/java/com/yisu/turbine/FwTurbineApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
 
 /**
- * 基于feign
  * @Author xuyisu
  * @Date 2019/12/6
  */
