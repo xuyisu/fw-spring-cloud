eead1217bf0e2eff2123a43ea21f46aa08cf03d6
xuyisu <yisu.xu@baozun.com>
2020-01-03 18:52:29 +0800

    Zuul 拦截器顺序

--- fw-cloud-gateways/fw-cloud-gateways-zuul/src/main/java/com/yisu/gateways/zuul/config/ZuulConfig.java
+++ fw-cloud-gateways/fw-cloud-gateways-zuul/src/main/java/com/yisu/gateways/zuul/config/ZuulConfig.java
@@ -1,6 +1,7 @@
 package com.yisu.gateways.zuul.config;
 
 import com.yisu.gateways.zuul.filter.TokenFilter;
+import com.yisu.gateways.zuul.filter.ZuulFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,8 +15,13 @@ public class ZuulConfig {
     /**
      * Zuul 过滤器配置，如果不想启动，注释掉即可
      */
-//    @Bean
-//    public TokenFilter tokenFilter(){
-//        return new TokenFilter();
-//    }
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+
+    @Bean
+    public ZuulFilter zuulFilter(){
+        return new ZuulFilter();
+    }
 }
--- fw-cloud-gateways/fw-cloud-gateways-zuul/src/main/java/com/yisu/gateways/zuul/filter/ZuulFilter.java
+++ fw-cloud-gateways/fw-cloud-gateways-zuul/src/main/java/com/yisu/gateways/zuul/filter/ZuulFilter.java
@@ -1,8 +1,6 @@
 package com.yisu.gateways.zuul.filter;
 
-import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
-import com.netflix.zuul.exception.ZuulException;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 
@@ -11,11 +9,11 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * @author xuyisu
- * @description Token Filter
- * @date 2019/12/12
+ * @description Zuul Filter
+ * @date 2019/12/28
  */
 @Slf4j
-public class TokenFilter extends ZuulFilter {
+public class ZuulFilter extends com.netflix.zuul.ZuulFilter {
     @Override
     public String filterType() {
         return "pre"; // 在请求被路由之前调用
@@ -23,7 +21,7 @@ public class TokenFilter extends ZuulFilter {
 
     @Override
     public int filterOrder() {
-        return 0; // filter执行顺序，通过数字指定 ,优先级为0，数字越大，优先级越低
+        return 5; // filter执行顺序，通过数字指定 ,优先级为0，数字越大，优先级越低
     }
 
     @Override
@@ -35,11 +33,11 @@ public class TokenFilter extends ZuulFilter {
     public Object run() {
         RequestContext ctx = RequestContext.getCurrentContext();
         HttpServletRequest request = ctx.getRequest();
-        log.info("我是TokenFilter");
-        String token = request.getHeader("token");// 获取请求的参数
+        log.info("我是ZuulFilter");
+        String token = request.getHeader("name");// 获取请求的参数
 
-        // 如果有token参数并且token值为123456，才进行路由
-        if (StringUtils.isNotBlank(token) && token.equals("123456")) {
+        // 如果有name参数并且token值为zuul，才进行路由
+        if (StringUtils.isNotBlank(token) && token.equals("zuul")) {
             ctx.setSendZuulResponse(true); //对请求进行路由
             ctx.setResponseStatusCode(200);
             ctx.set("code", 1);
@@ -48,7 +46,7 @@ public class TokenFilter extends ZuulFilter {
             ctx.setResponseStatusCode(401);
             HttpServletResponse response = ctx.getResponse();
             response.setHeader("content-type", "text/html;charset=utf8");
-            ctx.setResponseBody("认证失败");
+            ctx.setResponseBody("请携带网关必须参数");
             ctx.set("code", 0);
         }
         return null;
