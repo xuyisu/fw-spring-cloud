f594532018cc710e22c022b485ea9944fc4ad0c0
xuyisu <yisu.xu@baozun.com>
2020-01-17 11:32:35 +0800

    lock

--- fw-cloud-springboot/fw-springboot-lock/src/main/java/com/yisu/lock/redis/config/RedissonConfig.java
+++ fw-cloud-springboot/fw-springboot-lock/src/main/java/com/yisu/lock/redis/config/RedissonConfig.java
@@ -25,4 +25,36 @@ public class RedissonConfig {
         RedissonClient redisson = Redisson.create(config);
         return redisson;
     }
+
+//    @Bean
+//    public RedissonClient redissonClient()
+//    {
+//        Config config = new Config();
+//        config.useSentinelServers()
+//                .addSentinelAddress("redis://127.0.0.1:26379")
+//                .addSentinelAddress("redis://127.0.0.2:26379")
+//                .addSentinelAddress("redis://127.0.0.3:26379")
+//                .setPassword(redissonPassword);
+//        RedissonClient redisson = Redisson.create(config);
+//        return redisson;
+//    }
+
+//    @Bean
+//    public RedissonClient redissonClient()
+//    {
+//        Config config = new Config();
+//        config.useClusterServers()
+//                // 集群状态扫描间隔时间，单位是毫秒
+//                .setScanInterval(2000)
+//                //cluster方式至少6个节点(3主3从，3主做sharding，3从用来保证主宕机后可以高可用)
+//                .addNodeAddress("redis://127.0.0.1:6379" )
+//                .addNodeAddress("redis://127.0.0.1:6380")
+//                .addNodeAddress("redis://127.0.0.1:6381")
+//                .addNodeAddress("redis://127.0.0.1:6382")
+//                .addNodeAddress("redis://127.0.0.1:6383")
+//                .addNodeAddress("redis://127.0.0.1:6384")
+//                .setPassword(redissonPassword);
+//        RedissonClient redisson = Redisson.create(config);
+//        return redisson;
+//    }
 }
