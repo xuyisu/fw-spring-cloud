28cf82ee3d4539218bdf4c7da9998c0518e0f6bf
xuyisu <yisu.xu@baozun.com>
2020-01-20 18:28:19 +0800

    增加测试配置

--- fw-cloud-gateways/fw-cloud-gateways-gateway-simple/src/main/java/com/yisu/gateways/gateway/FwGatewaySimpleApplication.java
+++ fw-cloud-gateways/fw-cloud-gateways-gateway-simple/src/main/java/com/yisu/gateways/gateway/FwGatewaySimpleApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 /**
  * @Author xuyisu
