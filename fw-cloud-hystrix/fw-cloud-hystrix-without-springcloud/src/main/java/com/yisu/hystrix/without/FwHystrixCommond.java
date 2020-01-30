d0a751122bea5cefbc656173e5fda2914ba5d96c
bore <www.964092342@qq.com>
2020-01-30 16:37:58 +0800

    profile config

--- fw-cloud-hystrix/fw-cloud-hystrix-without-springcloud/src/main/java/com/yisu/hystrix/without/FwHystrixCommond.java
+++ fw-cloud-hystrix/fw-cloud-hystrix-without-springcloud/src/main/java/com/yisu/hystrix/without/FwHystrixCommond.java
@@ -23,7 +23,7 @@ public class FwHystrixCommond extends HystrixCommand<String> {
         return this.name+":"+Thread.currentThread().getName();
     }
 
-//    public static void main(String[] args) {
+//    public static.assert void main(String[] args) {
 //        String test = new FwHystrixCommond("test").execute();
 //        System.out.println(test);
 //    }
--- fw-cloud-springboot/fw-springboot-international/src/main/java/com/yisu/international/config/I18nConfig.java
+++ fw-cloud-springboot/fw-springboot-international/src/main/java/com/yisu/international/config/I18nConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
@@ -21,27 +22,27 @@ public class I18nConfig extends WebMvcConfigurationSupport {
      * session区域解析器
      * @return
      */
-//    @Bean
-//    public LocaleResolver localeResolver() {
-//        SessionLocaleResolver resolver = new SessionLocaleResolver();
-//        resolver.setDefaultLocale(Locale.CHINA);
-//
-//        return resolver;
-//    }
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver resolver = new SessionLocaleResolver();
+        resolver.setDefaultLocale(Locale.CHINA);
+
+        return resolver;
+    }
 
 
     /**
      * cookie区域解析器
      * @return
      */
-    @Bean
-    public LocaleResolver localeResolver() {
-        CookieLocaleResolver slr = new CookieLocaleResolver();
-        //设置默认区域,
-        slr.setDefaultLocale(Locale.CHINA);
-        slr.setCookieMaxAge(3600);//设置cookie有效期.
-        return slr;
-    }
+//    @Bean
+//    public LocaleResolver localeResolver() {
+//        CookieLocaleResolver slr = new CookieLocaleResolver();
+//        //设置默认区域,
+//        slr.setDefaultLocale(Locale.CHINA);
+//        slr.setCookieMaxAge(3600);//设置cookie有效期.
+//        return slr;
+//    }
 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
