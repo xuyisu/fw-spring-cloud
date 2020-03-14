424e1b7a5d7fc3fe4d3bb6ca2b94520d255b49b9
xuyisu <964092342@qq.com>
2020-03-14 21:45:59 +0800

    nacos 配置修改

--- fw-cloud-config-center/fw-cloud-nacos-client/src/main/java/com/yisu/FwNacosClientApplication.java
+++ fw-cloud-config-center/fw-cloud-nacos-client/src/main/java/com/yisu/FwNacosClientApplication.java
@@ -2,13 +2,14 @@ package com.yisu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author xuyisu
  * @description
  * @date 2019/12/12
  */
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class FwNacosClientApplication {
     public static void main(String[] args) {
--- fw-cloud-config-center/fw-cloud-nacos-client/src/main/java/com/yisu/nacos/client/TestController.java
+++ fw-cloud-config-center/fw-cloud-nacos-client/src/main/java/com/yisu/nacos/client/TestController.java
@@ -1,6 +1,7 @@
 package com.yisu.nacos.client;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,13 +12,13 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/config")
-//@RefreshScope
+@RefreshScope
 public class TestController {
-    @Value("${useLocalCache:false}")
-    private boolean useLocalCache;
+    @Value("${fwcloud.config.name:test}")
+    private String configName;
 
     @RequestMapping("/get")
-    public boolean get() {
-        return useLocalCache;
+    public String get() {
+        return configName;
     }
 }
