7e93b1d109cd85f2290b3387435100c4b737e3d8
xuyisu <964092342@qq.com>
2020-03-29 20:17:01 +0800

    分布式事务lcn修改

--- fw-cloud-transaction/fw-cloud-transaction-lcn/fw-cloud-transaction-lcn-tcc/fw-cloud-transaction-lcn-tcc-order/src/main/java/com/yisu/transaction/tcc/order/service/impl/OrderServiceImpl.java
+++ fw-cloud-transaction/fw-cloud-transaction-lcn/fw-cloud-transaction-lcn-tcc/fw-cloud-transaction-lcn-tcc-order/src/main/java/com/yisu/transaction/tcc/order/service/impl/OrderServiceImpl.java
@@ -1,7 +1,5 @@
 package com.yisu.transaction.tcc.order.service.impl;
 
-import com.codingapi.txlcn.tc.annotation.TccTransaction;
-import com.codingapi.txlcn.tracing.TracingContext;
 import com.yisu.transacation.base.dao.enums.StatusEnum;
 import com.yisu.transacation.base.dao.model.FwTradeLog;
 import com.yisu.transacation.base.dao.service.FwTradeLogService;
@@ -10,8 +8,7 @@ import com.yisu.transaction.tcc.order.service.OrderService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.concurrent.ConcurrentHashMap;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @description 订单记录表-业务实现
@@ -28,9 +25,8 @@ public class OrderServiceImpl implements OrderService {
     @Autowired
     private RemoteSendServiceFeign remoteSendServiceFeign;
 
-    ConcurrentHashMap<String, Long> hashMap = new ConcurrentHashMap<>();
 
-    @TccTransaction
+    @Transactional
     @Override
     public void saveAndPayOrder(String productName) {
 
@@ -41,21 +37,9 @@ public class OrderServiceImpl implements OrderService {
         log.info("[订单状态{}]=>{},当前商品id=>{},商品名称=>{}",fwTradeLog.getOrderId(), StatusEnum.TWO.getDesc(),fwTradeLog.getProductId(),fwTradeLog.getProductName());
 
         remoteSendServiceFeign.sendOrder(fwTradeLog);
-        hashMap.put(TracingContext.tracing().groupId(), fwTradeLog.getId());
-//        int i=1/0;
     }
 
-    public void confirmSaveAndPayOrder(String productName){
-        log.info("当前产品{},事务执行成功",productName);
-        hashMap.remove(TracingContext.tracing().groupId());
-    }
 
-    public void cancelSaveAndPayOrder(String productName){
-        Long tradeLogId = hashMap.get(TracingContext.tracing().groupId());
-        log.info("当前产品{},事务执行失败，回滚tradeLogId为{}的数据", productName,tradeLogId);
-        fwTradeLogService.removeById(tradeLogId);
-        hashMap.remove(TracingContext.tracing().groupId());
-    }
 
 
 }
--- fw-cloud-transaction/fw-cloud-transaction-lcn/fw-cloud-transaction-lcn-tcc/fw-cloud-transaction-lcn-tcc-send/src/main/java/com/yisu/transaction/tcc/send/service/impl/SendServiceImpl.java
+++ fw-cloud-transaction/fw-cloud-transaction-lcn/fw-cloud-transaction-lcn-tcc/fw-cloud-transaction-lcn-tcc-send/src/main/java/com/yisu/transaction/tcc/send/service/impl/SendServiceImpl.java
@@ -1,6 +1,7 @@
 package com.yisu.transaction.tcc.send.service.impl;
 
-import com.codingapi.txlcn.tc.annotation.LcnTransaction;
+import com.codingapi.txlcn.tc.annotation.TccTransaction;
+import com.codingapi.txlcn.tracing.TracingContext;
 import com.yisu.transacation.base.dao.enums.StatusEnum;
 import com.yisu.transacation.base.dao.model.FwTradeLog;
 import com.yisu.transacation.base.dao.service.FwTradeLogService;
@@ -8,6 +9,8 @@ import com.yisu.transaction.tcc.send.service.SendService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * @description 商品表-业务实现
@@ -20,13 +23,27 @@ public class SendServiceImpl implements SendService {
 
     @Autowired
     private FwTradeLogService fwTradeLogService;
+    ConcurrentHashMap<String, Long> hashMap = new ConcurrentHashMap<>();
 
-    @LcnTransaction
+    @TccTransaction
     @Override
     public void sendOrder(FwTradeLog fwTradeLog) {
         fwTradeLog.setStatus(StatusEnum.THREE.getValue());
         fwTradeLog.setStatusDsc(StatusEnum.THREE.getDesc());
         fwTradeLogService.save(fwTradeLog);
         log.info("[订单状态{}]=>{},当前商品id=>{},商品名称=>{}",fwTradeLog.getOrderId(), StatusEnum.THREE.getDesc(),fwTradeLog.getProductId(),fwTradeLog.getProductName());
+        hashMap.put(TracingContext.tracing().groupId(), fwTradeLog.getId());
+    }
+
+    public void confirmSendOrder(FwTradeLog fwTradeLog){
+        log.info("当前产品{},事务执行成功",fwTradeLog.getProductName());
+        hashMap.remove(TracingContext.tracing().groupId());
+    }
+
+    public void cancelSendOrder(FwTradeLog fwTradeLog){
+        Long tradeLogId = hashMap.get(TracingContext.tracing().groupId());
+        log.info("当前产品{},事务执行失败，回滚tradeLogId为{}的数据", fwTradeLog.getProductName(),tradeLogId);
+        fwTradeLogService.removeById(tradeLogId);
+        hashMap.remove(TracingContext.tracing().groupId());
     }
 }
